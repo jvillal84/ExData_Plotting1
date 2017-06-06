85ecdb0f23181d6fbd592459fda188f3352bb937
jvillal84 <jvillal84@gmail.com>
2017-06-06 12:21:55 +0200

    .

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data <- read.csv2(unz(temp,"household_power_consumption.txt"), header = TRUE, na.strings = "?", dec = ".")
-unlink(temp)
+unlink(temp) 
 
 ## Format the date and filter by desired days
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
